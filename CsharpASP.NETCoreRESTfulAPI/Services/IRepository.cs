816a694ba1242a772a144cbf0636fa36678ea85b
unknown <nets.devs@gmail.com>
2020-11-03 17:20:35 +0200

    RESTful API

---
+++ CsharpASP.NETCoreRESTfulAPI/Services/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CsharpASP.NETCoreRESTfulAPI.Entities;
+using CsharpASP.NETCoreRESTfulAPI.Services;
+
+namespace CsharpASP.NETCoreRESTfulAPI.Services
+{
+    public interface IRepository
+    {
+        void AddGenre(Genre genre);
+        Task<List<Genre>> GetAllGenres();
+        Genre GetGenreById(int Id);
+    }
+}
